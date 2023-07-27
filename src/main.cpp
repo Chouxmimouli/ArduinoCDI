9ca213008b5a1602ce0ef1a045ad9c1eddfe2a82
Chouxmimouli <chaxmouxmiloux@proton.me>
2023-07-27 16:21:33 +0200

    New clear version

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+
 #define rev_limiter 21
 #define ignition_cut_time 20
 #define trigger_coil_angle 27
@@ -44,6 +45,11 @@ void uart_transmit_char(uint8_t data) {
   UDR0 = data;
 }
 
+void uart_transmit_uint16(uint16_t data) {
+  uart_transmit_char((data >> 8) & 0xFF); // Transmit the high byte
+  uart_transmit_char(data & 0xFF); // Transmit the low byte
+}
+
 void uart_transmit_string(const char* string) {
   for (int i = 0; string[i] != '\0'; i++)
     uart_transmit_char(string[i]);
@@ -54,13 +60,10 @@ void uart_transmit_string(const char* string) {
 ////////////////////////////////////////////////////////////////////////////
 
 int main() {
-  uint8_t map1[23] = { E_0, E_500, E_1000, E_1500, E_2000, E_2500, E_3000, E_3500, E_4000, E_4500, E_5000, E_5500, E_6000, E_6500, E_7000, E_7500, E_8000, E_8500, E_9000, E_9500, E_10000, E_10500, E_11000 };
-  uint32_t pulse_delay, curve_select;
-  uint8_t led = 0;
-  uint32_t time_prev_pulse = 0; // To store the time of the previous pulse
-  uint32_t time_current_pulse;  // To store the time of the current pulse
-  uint32_t pulse_interval;      // To store the time interval between pulses in microseconds
-  uint32_t rpm;                 // To store the calculated RPM
+  uint8_t ignition_map[23] = { E_0, E_500, E_1000, E_1500, E_2000, E_2500, E_3000, E_3500, E_4000, E_4500, E_5000, E_5500, E_6000, E_6500, E_7000, E_7500, E_8000, E_8500, E_9000, E_9500, E_10000, E_10500, E_11000 };
+  uint8_t led = 0, angle_difference, map_index;
+  uint32_t pulse_interval, delay_time;
+  float rpm;
 
   PORTB = 0b00000000;
   DDRB = 0b00100010;
@@ -70,62 +73,54 @@ int main() {
   uart_init();
 
   while (true) {
-    // Wait for the rising edge (HIGH state) of the pulse
+    // While pin 8 is LOW
     while ((PINB & 0b00000001) == 0b00000000);
+    // If pin 8 is HIGH
+    if ((PINB & 0b00000001) == 0b00000001){
+      pulse_interval = TCNT1;
+      TCNT1 = 0;
+     
+      // Convert pulse_interval >in microseconds< into rpm
+      rpm = 1 / pulse_interval / 60000000;
 
-    // Store the time of the rising edge
-    time_prev_pulse = micros();
+      // Rounds the rpm value
+      map_index = round(rpm / 500);
+     
+      // Calculate the delay >in microseconds< needed to ignite at the specified advence angle in ignition_map
+      angle_difference = trigger_coil_angle - ignition_map[map_index];
+      delay_time = pulse_interval / 360 * angle_difference;
 
-    // Wait for the falling edge (LOW state) of the pulse
-    while ((PINB & 0b00000001) == 0b00000001);
+      // Lighting the led up on pin 13 every x revolutions
+      led++;
+      if (led == 1) {
+        PORTB = 0b00100000;
+        led = 0;
+      }
 
-    // Store the time of the falling edge
-    time_current_pulse = micros();
-
-    // Calculate the time interval between pulses
-    pulse_interval = time_current_pulse - time_prev_pulse;
-
-    // Calculate RPM based on the pulse interval
-    // The formula is: RPM = (60,000,000 / (trigger_coil_angle * pulse_interval))
-    // Since we are using microsecond intervals, we multiply by 60,000,000 to convert microseconds to minutes
-    rpm = 60000000UL / (trigger_coil_angle * pulse_interval);
-
-    // Rest of the code remains the same
-    TCNT1 = 0;
-
-    int index(rpm) {
-    return RPM >= 0 ? static_cast<int>(RPM + 0.5) : static_cast<int>(RPM - 0.5);
-    }
-
-    pulse_delay *= trigger_coil_angle - map1[curve_select];
-    pulse_delay /= 360;
-
-    led++;
-    if (led == 1) {
-      PORTB = 0b00100000;
-      led = 0;
-    }
-    if (curve_select < 23) {
-      while (TCNT1 < pulse_delay){
-        // Check if RPM exceeds the rev_limiter threshold
-        if (curve_select >= rev_limiter) {
-          // Ignition cut logic
-          PORTB = 0b00000000;  // Turn off ignition
-          _delay_ms(ignition_cut_time);  // Keep ignition off for ignition_cut_time
-        } else {
-            if (PIND & 0b10111111) {
-            PORTB = 0b00000010; // regular ignition
-            }
+      // Rev limiter and ignition
+      if (map_index < 23) {
+        //Waits the amount of time specified in delay_time
+        while (TCNT1 < delay_time){
+          // Check if RPM exceeds the rev_limiter threshold
+          if (map_index >= rev_limiter) {
+            // Ignition cut logic
+            PORTB = 0b00000000;  // Turn off ignition
+            _delay_ms(ignition_cut_time);  // Keep ignition off for ignition_cut_time
+          } else {
+              if (PIND & 0b10111111) {
+              PORTB = 0b00000010; // regular ignition
+              }
+          }
+        }
+        // Time during which pin 9 will be high >microseconds<
+        while (TCNT1 < delay_time + 25){
+          PORTB = 0b00000000;
         }
       }
-      while (TCNT1 < pulse_delay + 25){
-        PORTB = 0b00000000;
-      }
-    }
 
-    // Transmit the value of curve_select via serial
-    uart_transmit_string("curve_select: ");
-    uart_transmit_char(curve_select + '0');
-    uart_transmit_string("\n");
+      // Transmit the value of map_index via serial
+      uart_transmit_char(map_index);
+      uart_transmit_string("\n");
+    }
   }
 }
