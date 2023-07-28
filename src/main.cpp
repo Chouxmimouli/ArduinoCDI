32fefa80b53c9960b362f2c220204dc57087dc57
Chouxmimouli <chaxmouxmiloux@proton.me>
2023-07-28 15:56:47 +0200

    V.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,26 @@
 #include <Arduino.h>
 #include <avr/io.h>
 
-#define rev_limiter 10500 // rpm value
-#define ignition_cut_time 20000 // >microseconds< // 1ms = 1000μs
+#define rev_limiter 10500 // Rpm value
+#define ignition_cut_time 20000 // >Microseconds< // 1ms = 1000μs
 #define trigger_coil_angle 27
-#define E_0     10
-#define E_500   10
-#define E_1000  10
-#define E_1500  13
-#define E_2000  16
-#define E_2500  19
-#define E_3000  22
-#define E_3500  25
-#define E_4000  27
-#define E_4500  27
-#define E_5000  27
-#define E_5500  27
-#define E_6000  27
-#define E_6500  27
-#define E_7000  27
-#define E_7500  27
-#define E_8000  27
-#define E_8500  27
-#define E_9000  27
-#define E_9500  27
-#define E_10000 27
-#define E_10500 27
-#define E_11000 27
+#define RPM_0    10 // This curve is linear from 1000 RPM to 4000.
+#define RPM_250  10
+#define RPM_500  10
+#define RPM_750  10
+#define RPM_1000 10
+#define RPM_1250 11.4
+#define RPM_1500 12.8
+#define RPM_1750 14.2
+#define RPM_2000 15.6
+#define RPM_2250 17
+#define RPM_2500 18.5
+#define RPM_2750 19.9
+#define RPM_3000 21.3
+#define RPM_3250 22.7
+#define RPM_3500 24.1
+#define RPM_3750 25.5
+#define RPM_4000 27 // After this point, the curve becomes flat
 
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////Serial Communcation/////////////////////////////
@@ -58,10 +52,10 @@ void uart_transmit_string(const char* string) {
 ////////////////////////////////////////////////////////////////////////////
 
 int main() {
-  uint8_t ignition_map[23] = { E_0, E_500, E_1000, E_1500, E_2000, E_2500, E_3000, E_3500, E_4000, E_4500, E_5000, E_5500, E_6000, E_6500, E_7000, E_7500, E_8000, E_8500, E_9000, E_9500, E_10000, E_10500, E_11000 };
+  uint8_t ignition_map[17] = { RPM_0, RPM_250, RPM_500, RPM_750, RPM_1000, RPM_1250, RPM_1500, RPM_1750, RPM_2000, RPM_2250, RPM_2500, RPM_2750, RPM_3000, RPM_3250, RPM_3500, RPM_3750, RPM_4000 };
   uint8_t led = 0, angle_difference, map_index;
+  uint16_t rpm = 0; 
   uint32_t pulse_interval, delay_time;
-  float rpm;
 
   PORTB = 0b00000000;
   DDRB = 0b00100010;
@@ -75,14 +69,25 @@ int main() {
     while ((PINB & 0b00000001) == 0b00000000);
     // If pin 8 is HIGH
     if ((PINB & 0b00000001) == 0b00000001){
+      // Sets the value of the timer to pulse_interval >> time between 2 signals
       pulse_interval = TCNT1;
+      // Reset the timer
       TCNT1 = 0;
      
       // Convert pulse_interval >in microseconds< into rpm
-      rpm = 1 / pulse_interval / 60000000;
+      rpm = 60000000 / pulse_interval;
 
       // Rounds the rpm value
-      map_index = round(rpm / 500);
+      map_index = round(static_cast<double>(rpm) / 500);
+      
+      // Safety measure to limit the advence to 16 degrees >index 4< in case of an rpm sensor failure
+      if (map_index > 43) {
+        map_index = 9; //
+      }
+      // Caps the value of map_index to 16 because thats the last value in the ignition table above >RPM_4000 27<
+      else if (map_index > 16) {
+      map_index = 16;
+      }
      
       // Calculate the delay >in microseconds< needed to ignite at the specified advence angle in ignition_map
       angle_difference = trigger_coil_angle - ignition_map[map_index];
@@ -95,30 +100,30 @@ int main() {
         led = 0;
       }
 
-      // Rev limiter and ignition
-      if (map_index < 23) {
-        //Waits the amount of time specified in delay_time
-        while (TCNT1 < delay_time){
-          // Check if RPM exceeds the rev_limiter threshold
-          if (rpm >= rev_limiter) {
-            // Keep ignition off for ignition_cut_time
-            while (TCNT1 < ignition_cut_time) {
-              PORTB = 0b00000000;  // Turn off ignition
-            }
-          } 
-          else {
-            PORTB = 0b00000010; // regular ignition
+      //////// Rev limiter and ignition ////////
+      
+      // Waits the amount of time specified in delay_time
+      while (TCNT1 < delay_time){
+        // Check if RPM exceeds the rev_limiter threshold
+        if (rpm > rev_limiter) {
+          // Keep ignition off for ignition_cut_time
+          while (TCNT1 < ignition_cut_time) {
+            PORTB = 0b00000000;  // Turn off ignition
           }
+        } 
+        else {
+          PORTB = 0b00000010; // Ignition on pin 9
         }
       }
       // Time during which pin 9 will be high >microseconds<
       while (TCNT1 < delay_time + 25)
       PORTB = 0b00000000;
     }
-      // Transmit the value of map_index via serial
-      uart_transmit_char(map_index);
-      uart_transmit_string("\n");
+    // Transmit the value of map_index through serial >UART<
+    uart_transmit_char(map_index);
+    uart_transmit_string("\n");
+    uart_transmit_uint16(rpm);
+    uart_transmit_string("\n");
+    uart_transmit_string("\n");
   }
 }
-
-// To do : transmit rpm over UART >> convert float to uint16_t or int
