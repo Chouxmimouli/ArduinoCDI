80389f4e4a5bb48190122432aad80b5aebce1b31
Chouxmimouli <chaxmouxmiloux@proton.me>
2023-08-30 12:46:12 +0200

    back to the orignal exec system, timer OVF ISR

--- src/main.cpp
+++ src/main.cpp
@@ -65,79 +65,80 @@ void uart_transmit_uint(uint32_t data) {
 ////////////////////////////////Main Function///////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
+// Global variables
+boolean fresh_signal = true;
+
 int main() {
   float ignition_map[17] = { RPM_0, RPM_250, RPM_500, RPM_750, RPM_1000, RPM_1250, RPM_1500, RPM_1750, RPM_2000, RPM_2250, RPM_2500, RPM_2750, RPM_3000, RPM_3250, RPM_3500, RPM_3750, RPM_4000 };
   float angle_difference;
   uint8_t map_index;
   uint16_t rpm = 0, angle_difference2; 
   uint32_t pulse_interval, delay_time;
-  boolean can_run = false, rev_limiter_triggerd = false;
-
-  uint8_t pin_4_high = 0;
 
   PORTB = 0b00000000;
   DDRB = 0b00100100; // Set pin 10 and 13 as outputs and the rest are inputs
   PORTD = 0b00010000; // Enable pullup on pin 4
   DDRD = 0b00000000; // Set pin 0 to 7 as inputs
+  
   TCCR1B = 0b00000011; // 64 prescaler
+  TIMSK1 = 0b00000001; // Enable Timer1 overflow interrupt
 
+  sei(); // Enable global interrupts 
   uart_init();
 
   while (true) {
-    if ((PIND & 0b01000000) == 0b01000000 && can_run == true) {
-      // Set the value of the timer to pulse_interval >> time between 2 signals 
-      pulse_interval = TCNT1; 
-      
-      // Reset the timer
-      TCNT1 = 0;
+    while ((PIND & 0b01000000) == 0b00000000);
+    while ((PIND & 0b01000000) == 0b01000000);
     
+    // Set the value of the timer to pulse_interval >> time between 2 signals 
+    pulse_interval = TCNT1; 
+    
+    // Reset the timer
+    TCNT1 = 0;
+  
+    // When the revlimiter was active the previous rotation or when the timer overflows or on the first engine rotation, the rpm reading can be wrong
+    // becuase the prevous timer value is flawed ot inexistant. To fix the problem I use a variable called "fresh_signal" which sets the timer when 
+    // the arduino recieves a signal but prevent the fuel from being ingited
+    if (fresh_signal == false) {
+      
       // Convert pulse_interval into rpm
       rpm = 15000000 / pulse_interval;
-      
+
       // Round the rpm value
       map_index = round(rpm / 250.0);
-      
-      // Safety measure to limit the advence to 16 degrees >map_index 9< if the rpm reading exceeds 11000rpm (in case of an rpm sensor failure / timer overflow)
+
+      // Safety measure to limit the advence to 16 degrees >map_index 9< if the rpm reading exceeds 11000rpm (in case of an rpm sensor failure)
       if (map_index > 44) {
         map_index = 9;
       }
-      
+
       // Cap the value of map_index to 16 because thats the last value in the ignition table above >RPM_4000 27<
       else if (map_index > 16) {
       map_index = 16;
       }
-      
-      // When the revlimiter was active the previous rotation the rpm reading can be wrong, setting >map_index< to 16 is just a way to make sure that we 
-      // don't have 10 degrees of advence at high rpm 
-      else if (rev_limiter_triggerd == true) {
-        map_index = 16; 
-        rev_limiter_triggerd = false;
-      }
-      
+
       // Calculate the delay, not in μs, needed to ignite at the specified advence angle in ignition_map
       angle_difference = trigger_coil_angle - ignition_map[map_index];
       delay_time = pulse_interval / 360 * angle_difference;
       angle_difference2 = angle_difference * 10;
-      
+
       //////// Rev limiter and ignition ////////
       // Check if RPM exceeds the rev_limiter threshold
       if (rpm > rev_limiter) {
         // Keep ignition off for ignition_cut_time
         while (TCNT1 < ignition_cut_time);                 
-        rev_limiter_triggerd = true;
+        fresh_signal = true;
       } 
-      
       else { 
         // Wait the amount of time specified in delay_time
         while (TCNT1 < delay_time);
         PORTB = 0b00100100; // Ignition on pin 10
-        pin_4_high = 0;
+
+        // Time during which pin 10 will be high
+        while (TCNT1 < (delay_time + 50)); // 50*4 = >microseconds<
+        PORTB = PORTB & 0b11111011;
       }
 
-      // Time during which pin 10 will be high
-      while (TCNT1 < (delay_time + 250)); // 250*4 = >microseconds<
-      PORTB = PORTB & 0b11111011;
-      
       // Transmit the value of map_index through serial >UART<
       uart_transmit_uint(angle_difference2);
       uart_transmit_string("\n");
@@ -146,15 +147,19 @@ int main() {
       uart_transmit_uint(rpm);
       uart_transmit_string("\n");
       uart_transmit_string("\n");
-      
+
       //turn off the led
       PORTB = PORTB & 0b11011111;
-      
-      can_run = false;
     }
     else {
-      while ((PIND & 0b01000000) == 0b00000000);
-      can_run = true;
+      fresh_signal = false;
     }
   } 
+}
+
+ISR(TIMER1_OVF_vect) {
+  uart_transmit_string("Timer Overflow");
+  uart_transmit_string("\n");
+  uart_transmit_string("\n");
+  fresh_signal = true; 
 } 
